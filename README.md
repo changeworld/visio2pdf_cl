6cb4d1aa86fe722e23f20cb42caea0714e2cfb09
Takashi Takebayashi <t-takebayashi@m3.com>
2014-12-02 13:54:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# visio2pdf
+
+Visio to PDF format.
+
+## Usage
+
+1. git clone https://github.com/changeworld/visio2pdf.git
+2. cd visio2pdf
+3. ruby visio2pdf.rb [Existing directory of the visio file]
+
+## License
+
+Copyright (c) 2014 changewolrd
+
+https://github.com/changewolrd/visio2pdf/blob/master/LICENSE
---
+++ visio2pdf.rb
@@ -0,0 +1,39 @@
+#!ruby -Ks
+# -*- coding: utf-8 -*-
+require 'win32ole'
+require 'fileutils'
+
+def visio2pdf
+  if ARGV.length == 2
+    in_dir, out_dir = ARGV
+  else
+    return
+  end
+  visio = WIN32OLE.new('Visio.Application')
+  exts = ['.vsd']
+  begin
+    files = Dir["#{in_dir}/[^~]*{#{exts}}"]
+    files.sort.each_with_index do |file, i|
+      vsd_fullpath = File.expand_path(file)
+      pdf_fullpath = vsd_fullpath.gsub(/\.vsd$/, '.pdf')
+      begin
+        vsd = visio.Documents.Open(vsd_fullpath)
+        vsd.ExportAsFixedFormat({
+          'FixedFormat' => 1,
+          'OutputFileName' => pdf_fullpath,
+          'Intent' => 0,
+          'PrintRange' => 0
+        })
+        puts "success: #{vsd_fullpath}"
+      rescue
+        puts "fail: #{vsd_fullpath}"
+      ensure
+        vsd.Close
+      end
+    end
+  ensure
+    visio.Quit
+  end
+end
+
+visio2pdf()
