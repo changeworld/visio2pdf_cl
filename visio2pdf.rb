971381a9616155fc19986924cddf93ba9a04a271
Takashi Takebayashi <t-takebayashi@m3.com>
2014-12-03 10:28:18 +0900

    Fix Metrics/MethodLength: Max: 30 close #15

--- visio2pdf.rb
+++ visio2pdf.rb
@@ -1,39 +1,57 @@
 #!ruby -Ks
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 require 'win32ole'
 require 'fileutils'
 
-def visio2pdf
-  if ARGV.length == 2
-    in_dir, _out_dir = ARGV
-  else
-    return
-  end
-  visio = WIN32OLE.new('Visio.Application')
-  exts = ['.vsd']
-  begin
-    files = Dir["#{in_dir}/[^~]*{#{exts}}"]
-    files.sort.each do |file|
-      vsd_fullpath = File.expand_path(file)
-      pdf_fullpath = vsd_fullpath.gsub(/\.vsd$/, '.pdf')
-      begin
-        vsd = visio.Documents.Open(vsd_fullpath)
-        vsd.ExportAsFixedFormat(
-          'FixedFormat' => 1,
-          'OutputFileName' => pdf_fullpath,
-          'Intent' => 0,
-          'PrintRange' => 0
-        )
-        puts "success: #{vsd_fullpath}"
-      rescue
-        puts "fail: #{vsd_fullpath}"
-      ensure
-        vsd.Close
-      end
+# This class converts visio file into pdf file.
+class Visio2Pdf
+  VSDEXTS = '.vsd'
+  PDFEXTS = '.pdf'
+  @visio = nil
+  @in_dir = nil
+  @vsd_fullpath = nil
+  @pdf_fullpath = nil
+
+  def visio2pdf_controller
+    if ARGV.length == 2
+      @in_dir, @_out_dir = ARGV
+    else
+      return
     end
-  ensure
-    visio.Quit
+    visio2pdf
+  end
+
+  def visio2pdf
+    @visio = WIN32OLE.new('Visio.Application')
+    begin
+      files = Dir["#{@in_dir}/[^~]*{#{VSDEXTS}}"]
+      files.sort.each do |file|
+        get_filepath file
+        visio2pdf_exec
+      end
+    ensure
+      @visio.Quit
+    end
+  end
+
+  def visio2pdf_exec
+    vsd = @visio.Documents.Open(@vsd_fullpath)
+    begin
+      vsd.ExportAsFixedFormat(
+        FixedFormat: 1, OutputFileName: @pdf_fullpath, Intent: 0, PrintRange: 0
+      )
+    rescue
+      puts "fail: #{@vsd_fullpath}"
+    ensure
+      vsd.Close
+    end
+  end
+
+  def get_filepath(file)
+    @vsd_fullpath = File.expand_path(file)
+    @pdf_fullpath = @vsd_fullpath.gsub(/\.vsd$/, PDFEXTS)
   end
 end
 
-visio2pdf
+exec = Visio2Pdf.new
+exec.visio2pdf_controller
